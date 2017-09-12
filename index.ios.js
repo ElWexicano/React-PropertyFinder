3c2c69d03b7bd0ca5687776a94f0ac290000931b
elwexicano <shanebryandoyle@gmail.com>
2017-09-12 11:16:18 +0100

    Added more files.

--- index.ios.js
+++ index.ios.js
@@ -4,49 +4,33 @@
  * @flow
  */
 
+'use strict';
 import React, { Component } from 'react';
 import {
+  NavigatorIOS,
   AppRegistry,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+import SearchPage from './SearchPage';
 
-export default class PropertyFinder extends Component {
+class PropertyFinder extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  flex: 1,
   },
 });
 
